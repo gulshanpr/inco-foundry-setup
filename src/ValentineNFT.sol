85ead110d87d1ddcd895730ef6a6f0cf277dbc1d
gulshanpr <gulshankumarprasad04@gmail.com>
2025-02-13 22:12:19 +0530

    CERC20 test done

---
+++ src/ValentineNFT.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "fhevm/lib/TFHE.sol";
+import {Ownable2Step} from "@openzeppelin/contracts/access/Ownable2Step.sol";
+import {GatewayCaller} from "fhevm/gateway/GatewayCaller.sol";
+
+contract ValentineNFT is GatewayCaller {
+    
+}
