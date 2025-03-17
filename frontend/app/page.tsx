387f1697ac0dfc6244a16249cb92166d73419d50
gulshanpr <gulshankumarprasad@proton.me>
2025-03-18 00:37:54 +0530

    wrote follow along fracaster frame

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,25 +1,13 @@
 "use client";
-import { useEffect, useState } from 'react';
-import sdk from '@farcaster/frame-sdk';
+
+import Demo from "./demo";
 
 export default function Home() {
-
-  const [isSDKLoaded, setIsSDKLoaded] = useState(false);
-
-  useEffect(() => {
-    const load = async () => {
-      sdk.actions.ready();
-    };
-    if (sdk && !isSDKLoaded) {
-      setIsSDKLoaded(true);
-      load();
-    }
-  }, [isSDKLoaded]);
 
   return (
     <main className="min-h-screen flex flex-col p-4">
       <div className="w-[300px] mx-auto py-4 px-2">
-        <h1 className="text-2xl font-bold text-center mb-4">Frames for FHE based private nft metadata</h1>
+        <Demo/>
       </div>
     </main>
   );
